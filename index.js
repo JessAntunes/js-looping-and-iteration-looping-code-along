13cbf6a176eb6e07bc74dede04ff46840824271e
Jessica Antunes <jessantunes@protonmail.com>
2021-05-29 15:26:09 -0400

    commit tree 8b328a791237ae421118b6326f425a0f113d840b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, eventName) {
+    const array1 = [];
+    for (var i = 0; i < array.length; i++) {
+        array1.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return array1;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
